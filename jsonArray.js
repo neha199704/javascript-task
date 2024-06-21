f96b8df3f310d9d7af91e7afc54f6439c0e4f7b3
neha199704 <neharawat0408@gmail.com>
2024-06-21 13:35:52 +0530

    search input work properly

--- jsonArray.js
+++ jsonArray.js
@@ -3,45 +3,20 @@ console.log("hello");
 let btn = document.getElementById("btn");
 let ulList = document.getElementById("ul-list");
 let result = [];
+document.getElementById("input-search").addEventListener("input", search);
 
 function search() {
-  //let searching = result.filter(input.value);
   let input = document.getElementById("input-search").value.toLowerCase();
-  let searching = result.filter((item) => {
-    return item.title.toLowerCase().includes(input);
-    // if (item.title.toLowerCase().includes(input)) {
-    //   console.log("right");
-    // } else console.log("wrong");
-  });
+  let searching;
 
-  ulList.innerHTML = "";
-
-  for (let i = 0; i < searching.length; i++) {
-    // console.log(data);
-
-    const divElem = document.createElement("div");
-    const h2Elem = document.createElement("h2");
-    const imgElem = document.createElement("img");
-    const para = document.createElement("p");
-    const para1 = document.createElement("p");
-
-    h2Elem.innerText = searching[i].title;
-    //console.log(h2Elem);
-
-    imgElem.src = searching[i].image;
-    imgElem.classList.add("img");
-    imgElem.style.width = "200px";
-
-    para1.innerText = searching[i].description;
-    para.innerText = `Ratings ${searching[i].rating.rate} 
-     Rs. ${searching[i].price}`;
-
-    divElem.appendChild(h2Elem);
-    divElem.appendChild(imgElem);
-    divElem.appendChild(para1);
-    divElem.appendChild(para);
-    ulList.appendChild(divElem);
+  if (input === "") {
+    searching = result; // Show all products if input is empty
+  } else {
+    searching = result.filter((item) => {
+      return item.title.toLowerCase().includes(input);
+    });
   }
+  sendData(searching);
 
   console.log(result, "result");
   console.log(searching);
@@ -54,7 +29,11 @@ const products = async () => {
   let data = await response.json();
   console.log(data);
   result = data;
+  sendData(data);
+};
 
+function sendData(data) {
+  ulList.innerHTML = "";
   for (let i = 0; i < data.length; i++) {
     // console.log(data);
 
@@ -81,6 +60,6 @@ const products = async () => {
     divElem.appendChild(para);
     ulList.appendChild(divElem);
   }
-};
+}
 
 products();
